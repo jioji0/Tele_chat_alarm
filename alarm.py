17427a4c506e50382bee1e1d55f97df3f315368d
jy <jiyoung@kyonggi.ac.kr>
2023-11-22 01:40:48 +0900

    schedul add time

--- alarm.py
+++ alarm.py
@@ -1,6 +1,14 @@
 import telegram
 import asyncio
 
+token = "5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"
+bot = telegram.Bot(token=token)
+public_chat_name = '@dvnlb2'
+message = asyncio.run(bot.sendMessage(chat_id=public_chat_name, text="alarm arrived!"))
+print(message)
+
+
+'''
 async def send_message():
     token = "5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"
     bot = telegram.Bot(token=token)
@@ -10,7 +18,7 @@ async def send_message():
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(send_message())
-
+'''
 
 '''
 token = "5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"  # 여기에 봇 토큰을 넣으십시오.
--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,37 @@
+import sys
+import io
 import schedule
+import datetime
 import time
 import telegram
 import asyncio
+import pytz
+
+count = 1
 
 def job():
-    now = time.localtime()
+    
+    global count
+    count += 1
+    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    if now.hour >= 23 or now.hour <= 6:
+        return
+    
+    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+
     token = "5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"
     bot = telegram.Bot(token=token)
     public_chat_name = '@dvnlb2'
-    asyncio.run(bot.sendMessage(chat_id=public_chat_name, text="hi:)"))
+    asyncio.run(bot.sendMessage(chat_id=public_chat_name, text="schedule"))
     print("current time = ", str(now))
 
-schedule.every(1).minutes.do(job)
+#n분마다 실행
+schedule.every(0.1).minutes.do(job)
 
+print("Start App")
+
+#파이썬 스케쥴러
 while True:
     schedule.run_pending()
     time.sleep(1)
