74af1a832c82a26faae0145543ae51e5a38bd3ad
jioji0 <86821510+jioji0@users.noreply.github.com>
2023-11-24 09:22:13 +0900

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,32 +1,32 @@
+
 import telegram
 import asyncio
 
-token ="5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"  # 여기에 봇 토큰을 넣으십시오.
-chat_id = "6507308870"  # 여기에 채팅 ID를 넣으십시오.
+'''
+#최종 해결한 코드
+token =""  # 여기에 봇 토큰을 넣으십시오.
+chat_id = ""  # 여기에 채팅 ID를 넣으십시오.
 text = 'test'
-    
+
 bot = telegram.Bot(token=token)
 asyncio.run(bot.send_message(chat_id=chat_id, text=text))
-
-#최종 해결한 코드
+'''
 
 
 
 #처음 시도한 코드
-'''
-token ="5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"  # 여기에 봇 토큰을 넣으십시오.
-chat_id = "6507308870"  # 여기에 채팅 ID를 넣으십시오.
+token =""  # 여기에 봇 토큰을 넣으십시오.
+chat_id = ""  # 여기에 채팅 ID를 넣으십시오.
 text = 'test'
     
 bot = telegram.Bot(token=token)
 bot.send_message(chat_id=chat_id, text=text)
-'''
 
 #gpt 제안 코드
 '''
 async def send_message():
-    token ="5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"  # 여기에 봇 토큰을 넣으십시오.
-    chat_id = "6507308870"  # 여기에 채팅 ID를 넣으십시오.
+    token =""  # 여기에 봇 토큰을 넣으십시오.
+    chat_id = ""  # 여기에 채팅 ID를 넣으십시오.
     text = 'test'
     
     bot = telegram.Bot(token=token)
