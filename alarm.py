2994405cff421ca4c693d268bbe6da5f7f50a664
jioji0 <86821510+jioji0@users.noreply.github.com>
2023-11-24 09:21:00 +0900

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,30 +1,22 @@
 import telegram
 import asyncio
 
-token = "5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"
-bot = telegram.Bot(token=token)
-public_chat_name = '@dvnlb2'
-message = asyncio.run(bot.sendMessage(chat_id=public_chat_name, text="alarm arrived!"))
-print(message)
-
-
-'''
 async def send_message():
-    token = "5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"
+    token = ""  # 여기에 봇 토큰을 넣으십시오.
     bot = telegram.Bot(token=token)
-    public_chat_name = '@dvnlb2'
+    public_chat_name = '@abcde' # 여기에 chat_name을 입략하시오.
     message = await bot.sendMessage(chat_id=public_chat_name, text="alarm arrived!")
     print(message)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(send_message())
-'''
+
 
 '''
-token = "5965897880:AAHwEJqt5Axjg_TpL3c-fys78nJ7vFRqeF0"  # 여기에 봇 토큰을 넣으십시오.
-#chat_id = "6507308870"
+token = ""  # 여기에 봇 토큰을 넣으십시오.
+#chat_id = "123456"  #여기에 chat_id를 넣으십시오.
 bot = telegram.Bot(token = token)
-public_chat_name = '@dvnlb2'
+public_chat_name = '@abcde'  # 여기에 chat_name을 입략하시오.
 id_channel = bot.sendMessage(chat_id = public_chat_name, text = "alarm arrived!").chat_id
 print(id_channel)
 '''
