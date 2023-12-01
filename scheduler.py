900e08d2bd5ede2cb7258bf7460bc3f7aad4d1fe
jioji0 <86821510+jioji0@users.noreply.github.com>
2023-12-01 10:13:12 +0900

    Update scheduler.py
    설명 수정

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,29 @@
+'''비동기로 해결한 코드
+import schedule
+import asyncio
+import time
+import telegram
+
+async def job():
+    now = time.localtime()
+    token = ""  # 여기에 봇 토큰을 넣으십시오.
+    bot = telegram.Bot(token=token)
+    public_chat_name = '@abcde' # 여기에 chat_name을 입략하시오.
+    await bot.sendMessage(chat_id=public_chat_name, text="hi:)")
+    print("current time = ", str(now))
+
+async def main():
+    schedule.every(1).minutes.do(job)
+
+    while True:
+        await schedule.run_pending()
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+'''
+
 import sys
 import io
 import datetime
@@ -8,6 +34,7 @@ import telegram
 
 count = 1
 
+#버전을 다운그레이드해서 사용한 코드드
 def job():
     global count
     count += 1
@@ -34,28 +61,4 @@ while True:
     time.sleep(1)
 
 
-'''
-import schedule
-import asyncio
-import time
-import telegram
 
-async def job():
-    now = time.localtime()
-    token = ""  # 여기에 봇 토큰을 넣으십시오.
-    bot = telegram.Bot(token=token)
-    public_chat_name = '@abcde' # 여기에 chat_name을 입략하시오.
-    await bot.sendMessage(chat_id=public_chat_name, text="hi:)")
-    print("current time = ", str(now))
-
-async def main():
-    schedule.every(1).minutes.do(job)
-
-    while True:
-        await schedule.run_pending()
-        await asyncio.sleep(1)
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-'''
